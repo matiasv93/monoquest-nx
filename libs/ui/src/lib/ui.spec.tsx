3f03884399d303b145e3d7c1f8603619c276a086
Matias Vazquez <matiasvazquez93@gmail.com>
2025-07-01 21:20:42 +0200

    fix tailwind config

---
+++ libs/ui/src/lib/ui.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import MonoquestNxUi from './ui';
+
+describe('MonoquestNxUi', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<MonoquestNxUi />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function MonoquestNxUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to MonoquestNxUi!</h1>
+    </div>
+  );
+}
+
+export default MonoquestNxUi;
