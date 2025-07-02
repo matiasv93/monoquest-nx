09b72a36587e05a066f4307cbe108f7b5b2a4244
Matias Vazquez <matiasvazquez93@gmail.com>
2025-07-02 13:56:56 +0200

    remove example component

--- libs/ui/src/components/ui/ui.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './ui.module.css';
-
-export function MonoquestNxUi() {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to MonoquestNxUi!</h1>
-    </div>
-  );
-}
