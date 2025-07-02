e71f78960834115f2c111e45cbe4c7870c8aaf9e
Matias Vazquez <matiasvazquez93@gmail.com>
2025-07-02 19:28:39 +0200

    add BreadcrumbProvider as app wrapper

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,32 +1,13 @@
-import { Routes, Link } from 'react-router';
-import { Button } from '@monoquest-nx/ui';
+import { Routes } from 'react-router';
 
 import { AppRoutes } from './routes';
+import { BreadcrumbProvider } from './contexts/BreadcrumbContext';
 
 export function App() {
   return (
-    <>
-      <Button onClick={() => alert('Button from UI lib clicked!')}>
-        Randomize breadcrumb labels!
-      </Button>
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+    <BreadcrumbProvider>
       <Routes>{AppRoutes}</Routes>
-    </>
+    </BreadcrumbProvider>
   );
 }
 
